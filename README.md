7f35ec50908d6660e767d37a886eab86d6bba132
Sam Corbett <sam.corbett@cloudsoftcorp.com>
2013-08-12 18:33:44 +0100

    Live tests look for credentials in brooklyn properties, then system properties

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 brooklyn-ravello
 ================
+Linking Brooklyn and Ravello.
+
+Required properties:
+
+```ravello.username
+ravello.password```
+
+Give these as `-D` parameters to `mvn`.
+
+In brooklyn.properties:
+`brooklyn.location.named.ravello.<prop> = <value>`
--- src/test/java/io/cloudsoft/ravello/client/LiveTest.java
+++ src/test/java/io/cloudsoft/ravello/client/LiveTest.java
@@ -1,25 +1,39 @@
 package io.cloudsoft.ravello.client;
 
-import static com.google.common.base.Preconditions.checkNotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Optional;
+
+import brooklyn.config.BrooklynProperties;
 import io.cloudsoft.ravello.api.RavelloApi;
 
 public class LiveTest {
 
-    private static final String USER_PROPERTY = "ravello.username";
-    private static final String PASSWORD_PROPERTY = "ravello.password";
+    private static final Logger LOG = LoggerFactory.getLogger(LiveTest.class);
 
-    final String endpoint = "https://cloud.ravellosystems.com/services";
-    final String username;
-    final String password;
-    final RavelloApi ravello;
+    protected final String endpoint = "https://cloud.ravellosystems.com/services";
+    protected final RavelloApi ravello;
 
-    LiveTest() {
-        username = checkNotNull(System.getProperty(USER_PROPERTY),
-                "Expect value for "+USER_PROPERTY+" system property");
-        password = checkNotNull(System.getProperty(PASSWORD_PROPERTY),
-                "Expect value for " + PASSWORD_PROPERTY + " system property");
-        ravello = new RavelloApiImpl(endpoint, username, password);
+    /** Looks for Ravello credentials in brooklyn.properties, then in system properties. */
+    public LiveTest() {
+        BrooklynProperties properties = BrooklynProperties.Factory.newDefault();
+        Optional<String> username = Optional.fromNullable(properties.getFirst("brooklyn.location.named.ravello.username"));
+        Optional<String> password = Optional.fromNullable(properties.getFirst("brooklyn.location.named.ravello.password"));
+
+        if (!username.isPresent() || !password.isPresent()) {
+            LOG.info("Didn't find Ravello credentials in brooklyn properties, looking in system properties instead.");
+            username = Optional.fromNullable(System.getProperty("ravello.username"));
+            password = Optional.fromNullable(System.getProperty("ravello.password"));
+        } else {
+            LOG.info("Found Ravello credentials in brooklyn properties");
+        }
+
+        if (!username.isPresent() || !password.isPresent()) {
+            throw new RuntimeException("Expect Ravello username and password to be given in brooklyn or system properties");
+        }
+
+        ravello = new RavelloApiImpl(endpoint, username.get(), password.get());
     }
 
 }
